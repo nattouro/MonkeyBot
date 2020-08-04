29bc58b49a85a229854e19e9a59bbb5775824929
Nocturn Naps <48245084+NocturnNaps@users.noreply.github.com>
2020-08-04 23:08:52 +0300

    s

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ bot.on('ready' ,() =>{
     console.log('The bot is Online');
 })
 
-bot.on('message', mssage=>{
+bot.on('message', msg=>{
     if (msg.author.id == bot.user.id) {return;}
-    msg = mssage.content.toLowerCase();
-    if(msg.includes("pie") && msg.includes("furry"))
+    if(msg.content.includes("pie") && msg.content.includes("furry"))
     {
         msg.channel.send('furry.');
     }
-    if(msg.includes("salami"))
+    if(msg.content.includes("salami"))
     {
         msg.channel.send('aint gonna fit (wont fit) https://cdn.discordapp.com/attachments/544377900678381569/735086178344239104/video0.mp4')
     }
-    if(msg.includes("banana"))
+    if(msg.content.includes("banana"))
     {
         msg.channel.send('OOO OOO AAA AA AA BANANAA OOO OO')
     }
-    if(msg.includes("why"))
+    if(msg.content.includes("why"))
     {
         msg.channel.send('ur mom lol')
     }
-    if(msg.includes("monkey") )
+    if(msg.content.includes("monkey") )
     {
         msg.channel.send("*monkey sounds*")
     }
