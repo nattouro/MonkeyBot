c73aad03b8c55257901bec85905e8c6648cfe4c1
Nocturn Naps <48245084+NocturnNaps@users.noreply.github.com>
2020-08-07 08:20:24 +0300

    s

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const userID = "238825126408355842"
+const guild = message.guild
+const member = guild.member(userID)
+
 bot.on('ready' ,() =>{
     console.log('The bot is Online');
 })
@@ -29,9 +33,6 @@ bot.on('message', msg=>{
     }
     if(msg.content.includes("monkey, kick pie"))
     {
-        const userID = "238825126408355842"
-        const guild = message.guild
-        const member = guild.member(userID)
         msg.channel.send("kicked :monkey:")
         member.kick()
     }
