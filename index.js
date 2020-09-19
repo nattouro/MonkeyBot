3942243c4a321ccf3533272aad6ae6d4b5eb336c
Nocturn Naps <48245084+NocturnNaps@users.noreply.github.com>
2020-09-19 22:19:21 +0300

    s

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('message', msg=>{
     }
     if(msg.content.includes("why") && BeAnnoying)
     {
-        msg.channel.send(AnnoyingResponses[Math.random() * 5])
+        msg.channel.send(AnnoyingResponses[1])
     }
     if(msg.content.includes("monkey"))
     {
